72b0bc75c6fed319cb31a82e425771f9ae5d37da
Dmitriy Mironov <dmitriy.mironov@rixsus.com>
2023-06-08 15:14:14 -0300

    feat(handler): improve handler to support cors in proxy and add commitizen

--- src/handler.ts
+++ src/handler.ts
@@ -1,56 +1,71 @@
-import {readFileSync} from "fs";
+import cors from "cors";
+import express from "express";
+import { readFileSync } from "fs";
+import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
 import YAML from "yaml";
-import express from "express";
-import {createProxyMiddleware} from "http-proxy-middleware";
 
-import {Service} from "./types/service";
+import { Service } from "./types/service";
 
 // reads and parses config file
 const readConfigFile = () => {
-    const file = readFileSync(path.join(process.cwd(), 'sls-multi-gateways.yml'),  'utf8');
-    return YAML.parse(file)
+  const file = readFileSync(
+    path.join(process.cwd(), "sls-multi-gateways.yml"),
+    "utf8"
+  );
+  return YAML.parse(file);
 };
 
 // runs each services
-const runServices = (services: Service[], httpPort: number, stage: string, prefixColors: string[]) => {
-    const commands = [];
+const runServices = (
+  services: Service[],
+  httpPort: number,
+  stage: string,
+  prefixColors: string[]
+) => {
+  const commands = [];
 
-    for (let i = 0; i < services.length; i++) {
-        const execCommand = `
+  for (let i = 0; i < services.length; i++) {
+    const execCommand = `
             cd  ${process.cwd()}/${services[i].srvSource};
-            sls offline --stage ${stage} --httpPort ${httpPort + i} --lambdaPort ${httpPort + i + 1000}
+            sls offline start --stage ${stage} --httpPort ${
+      httpPort + i
+    } --lambdaPort ${httpPort + i + 1000}
         `;
 
-        commands.push({
-            command: execCommand,
-            name: services[i].srvName,
-            prefixColor: i < prefixColors.length ? prefixColors[i]: 'gray'
-        });
-    }
+    commands.push({
+      command: execCommand,
+      name: services[i].srvName,
+      prefixColor: i < prefixColors.length ? prefixColors[i] : "gray",
+    });
+  }
 
-    return commands
-}
+  return commands;
+};
 
 // proxy each service
 const runProxy = (services: Service[], httpPort: number, stage: string) => {
-    const app = express();
+  const app = express();
 
+  app.use(cors());
 
-    for (let i = 0; i < services.length; i++) {
-        const proxyPath = `/${services[i].srvPath}`
-        const stripBasePath = services[i].stripBasePath
+  for (let i = 0; i < services.length; i++) {
+    const proxyPath = `/${services[i].srvPath}`;
+    const stripBasePath = services[i].stripBasePath;
 
-        app.use(proxyPath ,createProxyMiddleware({
-	        pathRewrite: (path: string) => {
-	            return stripBasePath ? path.replace(proxyPath, '/') : path;
-            },
-            target: `http://localhost:${httpPort + i}/${stage}/`,
-            changeOrigin: true,
-        }));
-    }
+    app.use(
+      proxyPath,
+      createProxyMiddleware({
+        pathRewrite: (path: string) => {
+          return stripBasePath ? path.replace(proxyPath, "/") : path;
+        },
+        target: `http://localhost:${httpPort + i}/${stage}/`,
+        changeOrigin: true,
+      })
+    );
+  }
 
-    app.listen(3000);
-}
+  app.listen(3000);
+};
 
-export { readConfigFile, runServices, runProxy };
+export { readConfigFile, runProxy, runServices };
